542c6ad4a5c269a332327aebd3d218fa84c66650
mk0316 <mk0316@naver.com>
2024-06-03 14:34:51 +0900

    [feat]: 토큰으로 받아오기 (채팅방 목록, 채팅방 상세 기록, 손들기 알림 목록)

--- src/main/java/hello/community/domain/chat/ChatController.java
+++ src/main/java/hello/community/domain/chat/ChatController.java
@@ -1,24 +1,20 @@
 package hello.community.domain.chat;
 
-import hello.community.domain.community.CommunityDto;
 import hello.community.domain.user.UserDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @Tag(name = "Chat", description = "채팅 관련 API 입니다.")
 @RestController
@@ -32,18 +28,18 @@ public class ChatController {
             summary = "모집글에 손들기",
             description = "모집글에 손을 들어 참여를 신청합니다."
     )
-    public ResponseEntity<UserDto.CheckResult> raiseHand(@PathVariable Long groupBuyId) {
-        chatService.raiseHand(groupBuyId);
+    public ResponseEntity<UserDto.CheckResult> raiseHand(@PathVariable Long groupBuyId, @RequestHeader("Authorization") String token) {
+        chatService.raiseHand(groupBuyId, token);
         return ResponseEntity.ok(UserDto.CheckResult.builder().result("손들기 완료").build());
     }
 
-    @GetMapping("/notifications/{groupBuyId}")
+    @GetMapping("/notifications")
     @Operation(
             summary = "손들기 알림 목록 조회",
             description = "손들기 요청한 사용자의 정보를 배열로 조회합니다."
     )
-    public ResponseEntity<List<WaitingNotificationDto>> getWaitingNotifications(@PathVariable Long groupBuyId) {
-        List<WaitingNotificationDto> notifications = chatService.getWaitingNotifications(groupBuyId);
+    public ResponseEntity<List<WaitingNotificationDto>> getWaitingNotifications(@RequestHeader("Authorization") String token) {
+        List<WaitingNotificationDto> notifications = chatService.getWaitingNotifications(token);
         return ResponseEntity.ok(notifications);
     }
 
@@ -52,18 +48,18 @@ public class ChatController {
             summary = "손들기 수락",
             description = "대기자의 손들기 요청을 수락합니다."
     )
-    public ResponseEntity<UserDto.CheckResult> acceptWaiting(@PathVariable Long waitingId) {
-        chatService.acceptWaiting(waitingId);
+    public ResponseEntity<UserDto.CheckResult> acceptWaiting(@PathVariable Long waitingId, @RequestHeader("Authorization") String token) {
+        chatService.acceptWaiting(waitingId, token);
         return ResponseEntity.ok(UserDto.CheckResult.builder().result("수락 완료").build());
     }
 
-    @GetMapping("/chatrooms/{userId}")
+    @GetMapping("/chatrooms")
     @Operation(
             summary = "채팅방 목록 조회",
             description = "사용자가 참여하고 있는 채팅방 목록을 조회합니다."
     )
-    public ResponseEntity<List<ChatRoomListDto>> getChatRoomList(@PathVariable Long userId) {
-        List<ChatRoomListDto> chatRooms = chatService.getChatRoomList(userId);
+    public ResponseEntity<List<ChatRoomListDto>> getChatRoomList(@RequestHeader("Authorization") String token) {
+        List<ChatRoomListDto> chatRooms = chatService.getChatRoomList(token);
         return ResponseEntity.ok(chatRooms);
     }
 
@@ -72,28 +68,28 @@ public class ChatController {
             summary = "채팅 메시지 전송",
             description = "채팅방에 메시지를 전송합니다."
     )
-    public ResponseEntity<UserDto.CheckResult> sendMessage(@PathVariable Long chatRoomId, @RequestBody ChatMessageDto chatMessageDto) {
-        chatService.sendMessage(chatRoomId, chatMessageDto);
+    public ResponseEntity<UserDto.CheckResult> sendMessage(@PathVariable Long chatRoomId, @RequestBody ChatMessageDto chatMessageDto, @RequestHeader("Authorization") String token) {
+        chatService.sendMessage(chatRoomId, chatMessageDto, token);
         return ResponseEntity.ok(UserDto.CheckResult.builder().result("메시지 전송 완료").build());
     }
 
-    @GetMapping("/chatrooms/{chatRoomId}/records/{userId}")
+    @GetMapping("/chatrooms/{chatRoomId}/records")
     @Operation(
             summary = "채팅방 상세 기록 조회",
             description = "채팅방의 상세 채팅 기록을 조회합니다."
     )
-    public ResponseEntity<List<ChatRecordDto>> getChatRecords(@PathVariable Long chatRoomId, @PathVariable Long userId) {
-        List<ChatRecordDto> chatRecords = chatService.getChatRecords(chatRoomId, userId);
+    public ResponseEntity<List<ChatRecordDto>> getChatRecords(@PathVariable Long chatRoomId, @RequestHeader("Authorization") String token) {
+        List<ChatRecordDto> chatRecords = chatService.getChatRecords(chatRoomId, token);
         return ResponseEntity.ok(chatRecords);
     }
 
-    @DeleteMapping("/chatrooms/{chatRoomId}/users/{userId}")
+    @DeleteMapping("/chatrooms/{chatRoomId}/leave")
     @Operation(
             summary = "채팅방 나가기",
             description = "사용자가 특정 채팅방에서 나갑니다."
     )
-    public ResponseEntity<UserDto.CheckResult> leaveChatRoom(@PathVariable Long chatRoomId, @PathVariable Long userId) {
-        chatService.leaveChatRoom(chatRoomId, userId);
+    public ResponseEntity<UserDto.CheckResult> leaveChatRoom(@PathVariable Long chatRoomId, @RequestHeader("Authorization") String token) {
+        chatService.leaveChatRoom(chatRoomId, token);
         return ResponseEntity.ok(UserDto.CheckResult.builder().result("채팅방 나가기 완료").build());
     }
 
--- src/main/java/hello/community/domain/chat/ChatService.java
+++ src/main/java/hello/community/domain/chat/ChatService.java
@@ -7,18 +7,16 @@ import hello.community.domain.user.Users;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import hello.community.global.jwt.JwtService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 
 @Service
 @Transactional
@@ -33,174 +31,212 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final UserChatRoomRepository userChatRoomRepository;
     private final ChatRepository chatRepository;
+    private final JwtService jwtService;
 
     // 모집글 상세 액티비티 하단의 손들기 버튼 클릭 시 대기자 테이블에 요청자에 대한 튜플 추가
-    public void raiseHand(Long groupBuyId) {
-        GroupBuy findedGroupBuy = groupBuyRepository.findById(groupBuyId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 모집글이 존재하지 않습니다."));
+    public void raiseHand(Long groupBuyId, String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
+            Users user = userRepository.findById(userId)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        Long userId = Long.parseLong(username);
-        Users user = userRepository.findById(userId)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            GroupBuy findedGroupBuy = groupBuyRepository.findById(groupBuyId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 모집글이 존재하지 않습니다."));
 
-        if (findedGroupBuy.getUser().getId().equals(user.getId())) {
-            throw new IllegalArgumentException("본인이 작성한 모집글에는 참여할 수 없습니다.");
+            if (findedGroupBuy.getUser().getId().equals(user.getId())) {
+                throw new IllegalArgumentException("본인이 작성한 모집글에는 참여할 수 없습니다.");
+            }
+
+            Waiting waiting = new Waiting();
+            waiting.setUser(user);
+            waiting.setGroupBuy(findedGroupBuy);
+            waitingRepository.save(waiting);
+        } catch (Exception e) {
+            logger.error("Error raising hand", e);
+            throw new RuntimeException("Error raising hand", e);
         }
-
-        Waiting waiting = new Waiting();
-        waiting.setUser(user);
-        waiting.setGroupBuy(findedGroupBuy);
-        waitingRepository.save(waiting);
     }
 
     // 알림 목록 액티비티 내에서 손들기를 요청한 사용자의 정보를 배열로 조회
-    public List<WaitingNotificationDto> getWaitingNotifications(Long groupBuyId) {
-        List<Waiting> waitings = waitingRepository.findByGroupBuyId(groupBuyId);
-        return waitings.stream().map(waiting -> {
-            GroupBuy groupBuy = waiting.getGroupBuy();
-            return new WaitingNotificationDto(
-                    waiting.getUser().getName(),
-                    waiting.getUser().getPhoto(),
-                    groupBuy.getTitle(),
-                    groupBuy.getPhoto(),
-                    waiting.getCreatedAt(),
-                    groupBuy.getHeadCount(),
-                    groupBuy.getCurrentCount()
-            );
-        }).collect(Collectors.toList());
+    public List<WaitingNotificationDto> getWaitingNotifications(String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
+
+            List<GroupBuy> groupBuys = groupBuyRepository.findByUserId(userId);
+
+            return groupBuys.stream().flatMap(groupBuy -> {
+                List<Waiting> waitings = waitingRepository.findByGroupBuyId(groupBuy.getId());
+                return waitings.stream().map(waiting -> new WaitingNotificationDto(
+                        waiting.getUser().getName(),
+                        waiting.getUser().getPhoto(),
+                        groupBuy.getTitle(),
+                        groupBuy.getPhoto(),
+                        waiting.getCreatedAt(),
+                        groupBuy.getHeadCount(),
+                        groupBuy.getCurrentCount()
+                ));
+            }).collect(Collectors.toList());
+        } catch (Exception e) {
+            logger.error("Error getting waiting notifications", e);
+            throw new RuntimeException("Error getting waiting notifications", e);
+        }
     }
 
     // 손들기 수락 & 채팅방 생성
-    public void acceptWaiting(Long waitingId) {
-        // 대기자 수락 로직
-        Waiting waiting = waitingRepository.findById(waitingId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 대기자가 존재하지 않습니다."));
-        GroupBuy groupBuy = waiting.getGroupBuy();
+    public void acceptWaiting(Long waitingId, String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
 
-        waiting.setAccepted(true); // 상태 변경
-        waitingRepository.save(waiting);
+            // 대기자 수락 로직
+            Waiting waiting = waitingRepository.findById(waitingId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 대기자가 존재하지 않습니다."));
+            GroupBuy groupBuy = waiting.getGroupBuy();
 
-        // 현재 인원 수 업데이트
-        groupBuy.setCurrentCount(groupBuy.getCurrentCount() + 1);
-        groupBuyRepository.save(groupBuy);
+            waiting.setAccepted(true); // 상태 변경
+            waitingRepository.save(waiting);
 
-        // 채팅방 생성 조건 확인 및 생성
-        if (groupBuy.getCurrentCount() == groupBuy.getHeadCount()) {
-            ChatRoom chatRoom = new ChatRoom();
-            chatRoom.setGroupBuy(groupBuy);
-            chatRoomRepository.save(chatRoom);
+            // 현재 인원 수 업데이트
+            groupBuy.setCurrentCount(groupBuy.getCurrentCount() + 1);
+            groupBuyRepository.save(groupBuy);
 
-            // 채팅방에 사용자 추가
-            List<Waiting> acceptedWaitings = waitingRepository.findByGroupBuyIdAndAccepted(groupBuy.getId(), true);
-            for (Waiting acceptedWaiting : acceptedWaitings) {
-                UserChatRoom userChatRoom = new UserChatRoom();
-                userChatRoom.setUser(acceptedWaiting.getUser());
-                userChatRoom.setChatRoom(chatRoom);
-                userChatRoomRepository.save(userChatRoom);
+            // 채팅방 생성 조건 확인 및 생성
+            if (groupBuy.getCurrentCount() == groupBuy.getHeadCount()) {
+                ChatRoom chatRoom = new ChatRoom();
+                chatRoom.setGroupBuy(groupBuy);
+                chatRoomRepository.save(chatRoom);
+
+                // 채팅방에 사용자 추가
+                List<Waiting> acceptedWaitings = waitingRepository.findByGroupBuyIdAndAccepted(groupBuy.getId(), true);
+                for (Waiting acceptedWaiting : acceptedWaitings) {
+                    UserChatRoom userChatRoom = new UserChatRoom();
+                    userChatRoom.setUser(acceptedWaiting.getUser());
+                    userChatRoom.setChatRoom(chatRoom);
+                    userChatRoomRepository.save(userChatRoom);
+                }
+
+                // 파티장도 채팅방에 추가
+                UserChatRoom ownerChatRoom = new UserChatRoom();
+                ownerChatRoom.setUser(groupBuy.getUser());
+                ownerChatRoom.setChatRoom(chatRoom);
+                userChatRoomRepository.save(ownerChatRoom);
             }
-
-            // 파티장도 채팅방에 추가
-            UserChatRoom ownerChatRoom = new UserChatRoom();
-            ownerChatRoom.setUser(groupBuy.getUser());
-            ownerChatRoom.setChatRoom(chatRoom);
-            userChatRoomRepository.save(ownerChatRoom);
+        } catch (Exception e) {
+            logger.error("Error accepting waiting", e);
+            throw new RuntimeException("Error accepting waiting", e);
         }
     }
 
     // 채팅방 목록 액티비티 내에 채팅방 정보 목록이 배열로 조회
     // 채팅방 목록 조회 메서드 추가
-    public List<ChatRoomListDto> getChatRoomList(Long userId) {
-        List<UserChatRoom> userChatRooms = userChatRoomRepository.findByUserId(userId);
+    public List<ChatRoomListDto> getChatRoomList(String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
+            List<UserChatRoom> userChatRooms = userChatRoomRepository.findByUserId(userId);
 
-        return userChatRooms.stream().map(userChatRoom -> {
-            ChatRoom chatRoom = userChatRoom.getChatRoom();
-            List<String> participantImages = chatRoom.getUserChatRooms().stream()
-                    .map(uc -> uc.getUser().getPhoto())
-                    .collect(Collectors.toList());
-            GroupBuy groupBuy = chatRoom.getGroupBuy();
-            Chat lastChat = chatRepository.findTopByChatRoomOrderByCreatedAtDesc(chatRoom);
-            long unreadCount = chatRepository.countByChatRoomAndCreatedAtAfter(chatRoom, userChatRoom.getLastReadTime());
+            return userChatRooms.stream().map(userChatRoom -> {
+                ChatRoom chatRoom = userChatRoom.getChatRoom();
+                List<String> participantImages = chatRoom.getUserChatRooms().stream()
+                        .map(uc -> uc.getUser().getPhoto())
+                        .collect(Collectors.toList());
+                GroupBuy groupBuy = chatRoom.getGroupBuy();
+                Chat lastChat = chatRepository.findTopByChatRoomOrderByCreatedAtDesc(chatRoom);
+                long unreadCount = chatRepository.countByChatRoomAndCreatedAtAfter(chatRoom, userChatRoom.getLastReadTime());
 
-            return new ChatRoomListDto(
-                    participantImages, // 모든 참여자의 이미지 URL 리스트
-                    groupBuy.getPhoto(),
-                    groupBuy.getTitle(),
-                    chatRoom.getUserChatRooms().size(),
-                    lastChat != null ? lastChat.getContent() : "",
-                    lastChat != null ? lastChat.getCreatedAt() : null,
-                    (int) unreadCount
-            );
-        }).collect(Collectors.toList());
-    }
-
-    // 5. 채팅 메시지 전송
-    public void sendMessage(Long chatRoomId, ChatMessageDto chatMessageDto) {
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 채팅방이 존재하지 않습니다."));
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        Long userId = Long.parseLong(username);
-        Users user = userRepository.findById(userId)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Chat chat = new Chat();
-        chat.setContent(chatMessageDto.getContent());
-        chat.setCreatedAt(LocalDateTime.now());
-        chat.setUser(user);
-        chat.setChatRoom(chatRoom);
-        chatRepository.save(chat);
-
-        Optional<UserChatRoom> optionalUserChatRoom = userChatRoomRepository.findByUserIdAndChatRoomId(userId, chatRoomId);
-        UserChatRoom userChatRoom = optionalUserChatRoom.orElseThrow(() -> new IllegalArgumentException("해당 채팅방에 사용자가 존재하지 않습니다."));
-        userChatRoom.setLastReadTime(LocalDateTime.now());
-        userChatRoomRepository.save(userChatRoom);
-    }
-
-    // 6.채팅방 상세 액티비티 내에 사용자들의 채팅 기록
-    public List<ChatRecordDto> getChatRecords(Long chatRoomId, Long userId) {
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 채팅방이 존재하지 않습니다."));
-
-        List<Chat> chats = chatRepository.findByChatRoomOrderByCreatedAtAsc(chatRoom);
-        Users currentUser = userRepository.findById(userId)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        List<ChatRecordDto> chatRecords = new ArrayList<>();
-        Long previousUserId = null;
-        String chatRoomTitle = chatRoom.getGroupBuy().getTitle(); // 채팅방 제목 가져오기
-
-        for (Chat chat : chats) {
-            boolean isContinuousMessage = previousUserId != null && previousUserId.equals(chat.getUser().getId());
-            boolean isMyMessage = chat.getUser().getId().equals(currentUser.getId());
-
-            chatRecords.add(new ChatRecordDto(
-                    chat.getUser().getPhoto(),
-                    chat.getUser().getName(),
-                    chat.getContent(),
-                    chat.getCreatedAt(),
-                    isContinuousMessage,
-                    isMyMessage,
-                    chatRoomTitle // 채팅방 제목 추가
-            ));
-
-            previousUserId = chat.getUser().getId();
+                return new ChatRoomListDto(
+                        participantImages,
+                        groupBuy.getPhoto(),
+                        groupBuy.getTitle(),
+                        chatRoom.getUserChatRooms().size(),
+                        lastChat != null ? lastChat.getContent() : "",
+                        lastChat != null ? lastChat.getCreatedAt() : null,
+                        (int) unreadCount
+                );
+            }).collect(Collectors.toList());
+        } catch (Exception e) {
+            logger.error("Error getting chat room list", e);
+            throw new RuntimeException("Error getting chat room list", e);
         }
-
-        return chatRecords;
     }
 
-    // 7. 채팅방 나가시 시에, 채팅방 참여자 정보에서 해당 유저 데이터를 제거함.
+    // 채팅 메시지 전송
+    public void sendMessage(Long chatRoomId, ChatMessageDto chatMessageDto, String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
+            Users user = userRepository.findById(userId)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+            ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 채팅방이 존재하지 않습니다."));
+
+            Chat chat = new Chat();
+            chat.setContent(chatMessageDto.getContent());
+            chat.setCreatedAt(LocalDateTime.now());
+            chat.setUser(user);
+            chat.setChatRoom(chatRoom);
+            chatRepository.save(chat);
+
+            Optional<UserChatRoom> optionalUserChatRoom = userChatRoomRepository.findByUserIdAndChatRoomId(userId, chatRoomId);
+            UserChatRoom userChatRoom = optionalUserChatRoom.orElseThrow(() -> new IllegalArgumentException("해당 채팅방에 사용자가 존재하지 않습니다."));
+            userChatRoom.setLastReadTime(LocalDateTime.now());
+            userChatRoomRepository.save(userChatRoom);
+        } catch (Exception e) {
+            logger.error("Error sending message", e);
+            throw new RuntimeException("Error sending message", e);
+        }
+    }
+
+    // 채팅방 상세 기록 조회
+    public List<ChatRecordDto> getChatRecords(Long chatRoomId, String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
+            Users currentUser = userRepository.findById(userId)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+            ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 채팅방이 존재하지 않습니다."));
+
+            List<Chat> chats = chatRepository.findByChatRoomOrderByCreatedAtAsc(chatRoom);
+
+            List<ChatRecordDto> chatRecords = new ArrayList<>();
+            Long previousUserId = null;
+            String chatRoomTitle = chatRoom.getGroupBuy().getTitle(); // 채팅방 제목 가져오기
+
+            for (Chat chat : chats) {
+                boolean isContinuousMessage = previousUserId != null && previousUserId.equals(chat.getUser().getId());
+                boolean isMyMessage = chat.getUser().getId().equals(currentUser.getId());
+
+                chatRecords.add(new ChatRecordDto(
+                        chat.getUser().getPhoto(),
+                        chat.getUser().getName(),
+                        chat.getContent(),
+                        chat.getCreatedAt(),
+                        isContinuousMessage,
+                        isMyMessage,
+                        chatRoomTitle // 채팅방 제목 추가
+                ));
+
+                previousUserId = chat.getUser().getId();
+            }
+
+            return chatRecords;
+        } catch (Exception e) {
+            logger.error("Error getting chat records", e);
+            throw new RuntimeException("Error getting chat records", e);
+        }
+    }
+    // 채팅방 나가기
     @Transactional
-    public void leaveChatRoom(Long chatRoomId, Long userId) {
-        Optional<UserChatRoom> optionalUserChatRoom = userChatRoomRepository.findByUserIdAndChatRoomId(userId, chatRoomId);
-        UserChatRoom userChatRoom = optionalUserChatRoom.orElseThrow(() -> new IllegalArgumentException("해당 채팅방에 사용자가 존재하지 않습니다."));
+    public void leaveChatRoom(Long chatRoomId, String token) {
+        try {
+            Long userId = jwtService.getUserId(token);
+            Optional<UserChatRoom> optionalUserChatRoom = userChatRoomRepository.findByUserIdAndChatRoomId(userId, chatRoomId);
+            UserChatRoom userChatRoom = optionalUserChatRoom.orElseThrow(() -> new IllegalArgumentException("해당 채팅방에 사용자가 존재하지 않습니다."));
 
-        userChatRoomRepository.delete(userChatRoom);
+            userChatRoomRepository.delete(userChatRoom);
+        } catch (Exception e) {
+            logger.error("Error leaving chat room", e);
+            throw new RuntimeException("Error leaving chat room", e);
+        }
     }
-
-
 
 }
